be59da7c94aa8a4a9bc0e55d0d8ffa063b00163e
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-09 01:12:37 -0500

    typeDefs should be complete

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -34,12 +34,12 @@ const resolvers = {
 
             return { token, user };
         },
-        addBook: async (parent, { authors, description, bookID, image, link, title }, context) => {
+        saveBook: async (parent, { authors, description, bookId, image, link, title }, context) => {
             if (context.user) {
               const book = await Book.create({
                 authors,
                 description,
-                bookID,
+                bookId,
                 image,
                 link,
                 title
@@ -54,10 +54,10 @@ const resolvers = {
             }
             throw new AuthenticationError('You need to be logged in!');
           },
-          deleteBook: async (parent, { bookID }, context) => {
+          removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
               const book = await Book.findOneAndDelete({
-                _id: bookID,
+                _id: bookId,
               });
       
               await User.findOneAndUpdate(
